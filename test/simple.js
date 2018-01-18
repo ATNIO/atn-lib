777e6a5f605b0f035ade0618deaae7a8df672639
HuJingwei <a479540195@gmail.com>
2018-01-18 16:08:49 +0800

    test

--- test/simple.js
+++ test/simple.js
@@ -1,9 +1,9 @@
 const AtnLib = require('../')
-var test = require("tape")
+// var test = require("tape")
 
 var lib = new AtnLib(
   "c431bc51736dcacb1f89369fca31f2e252a08c3abe875aba6af607460f42986b",
-  // "https://kovan.infura.io/AMiQRoDABln2S7gObydS"
+  // "https://kovan.infura.io/"
   "ws://118.31.18.101:4046"
 );
 
@@ -12,65 +12,27 @@ var lib = new AtnLib(
 //   assert.end();
 // })
 
-// test("getAiInfo", 1, function (assert) {
-//   lib.getAiInfo(1);
-//   assert.end();
-// })
+async function run() {
+  await lib.getAiList();
+  await lib.getAiInfo(1);
+  await lib.openChannel('0x9765e2d8467334198b402e4d4551dd49e63327ec', 1);
+  await lib.getChannels('http://127.0.0.1:3000', 5437663);
+  await lib.topUpChannel('0x9765e2d8467334198b402e4d4551dd49e63327ec', 5437663, 1)
+  await lib.closeChannel('http://127.0.0.1:3000', "0x9765e2d8467334198b402e4d4551dd49e63327ec", 5138755, 0)
+  await lib.getChannels('http://127.0.0.1:3000', 5176704)
+  await lib.getPrice('http://127.0.0.1:3000', 'xiaoi')
+  await lib.callAI(
+    'http://127.0.0.1:3000',
+    'xiaoi',
+    'hi',
+    '0x9765e2d8467334198b402e4d4551dd49e63327ec',
+    5176704,
+    2,
+    1
+  )
+  await lib.directlyCallAI('XIAO_I', {
+    question: "你好!"
+  })
+}
 
-// test("openChannel", function (assert) {
-//   lib.openChannel('0x9765e2d8467334198b402e4d4551dd49e63327ec', 1);
-//   assert.end();
-// })
-
-// test("topUpChannel", function (assert) {
-//   lib.topUpChannel('0x9765e2d8467334198b402e4d4551dd49e63327ec', 5176704, 1);
-//   assert.end();
-// })
-
-// test("closeChannel", function (assert) {
-//   lib.closeChannel('http://127.0.0.1:3000', "0x9765e2d8467334198b402e4d4551dd49e63327ec", 5138755, 0);
-//   assert.end();
-// })
-
-// test("getChannels", function (assert) {
-//   lib.getChannels('http://127.0.0.1:3000', 5176704)
-//   assert.end();
-// })
-
-// test("getPrice", function (assert) {
-//   lib.getPrice('http://127.0.0.1:3000', 'xiaoi');
-//   assert.end();
-// })
-
-// test("callAI", function (assert) {
-//   lib.callAI(
-//     'http://127.0.0.1:3000',
-//     'xiaoi',
-//     'hi',
-//     '0x9765e2d8467334198b402e4d4551dd49e63327ec',
-//     5176704,
-//     2,
-//     1
-//   )
-//   assert.end();
-// })
-
-// lib.getAiList();
-// lib.getAiInfo(1);
-// lib.openChannel('0x9765e2d8467334198b402e4d4551dd49e63327ec', 1);
-// lib.topUpChannel('0x9765e2d8467334198b402e4d4551dd49e63327ec', 5176704, 1);
-// lib.closeChannel('http://127.0.0.1:3000',"0x9765e2d8467334198b402e4d4551dd49e63327ec", 5138755, 0)
-// lib.getChannels('http://127.0.0.1:3000', 5176704)
-// lib.getPrice('http://127.0.0.1:3000', 'xiaoi')
-// lib.callAI(
-//   'http://127.0.0.1:3000',
-//   'xiaoi',
-//   'hi',
-//   '0x9765e2d8467334198b402e4d4551dd49e63327ec',
-//   5176704,
-//   2,
-//   1
-// )
-lib.directlyCallAI('XIAO_I', {
-  question:"你好!"
-})
+run();
