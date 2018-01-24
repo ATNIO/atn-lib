ed39d044ec122ff771ef55374f9c086aece0a599
HuJingwei <a479540195@gmail.com>
2018-01-24 18:39:47 +0800

    sendTx

--- test/simple.js
+++ test/simple.js
@@ -3,7 +3,7 @@ const assert = require("assert")
 const lib = new AtnLib(
   "c431bc51736dcacb1f89369fca31f2e252a08c3abe875aba6af607460f42986b",
   // "https://kovan.infura.io/"
-  "http://118.31.18.101:4045"
+  "ws://118.31.18.101:4046"
 );
 
 // describe('atnLib', function() {
@@ -29,24 +29,24 @@ const lib = new AtnLib(
 // });
 
 async function run() {
-  await lib.openChannel('0x9765e2d8467334198b402e4d4551dd49e63327ec', 1);
-  // await lib.getChannels('http://127.0.0.1:4010', 5437663);
-  // await lib.topUpChannel('0x9765e2d8467334198b402e4d4551dd49e63327ec', 5437663, 1)
-  // await lib.closeChannel('http://127.0.0.1:3000', "0x9765e2d8467334198b402e4d4551dd49e63327ec", 5138755, 0)
-  // await lib.getPrice('http://127.0.0.1:4018', 'azureVision')
+  // await lib.openChannel('0x9765e2d8467334198b402e4d4551dd49e63327ec', 1);
+  await lib.getChannels('http://118.31.18.101:4010', 5537263);
+  // await lib.topUpChannel('0x9765e2d8467334198b402e4d4551dd49e63327ec', 5537263, 1)
+  // await lib.closeChannel('http://118.31.18.101:4010', "0x9765e2d8467334198b402e4d4551dd49e63327ec", 5537407, 0)
+  // await lib.getPrice('http://118.31.18.101:4010', 'xiaoi')
   // await lib.callAI(
-  //   'http://127.0.0.1:3000',
+  //   'http://118.31.18.101:4010',
   //   'xiaoi',
   //   'hi',
   //   '0x9765e2d8467334198b402e4d4551dd49e63327ec',
-  //   5176704,
-  //   2,
+  //   5537465,
+  //   0,
   //   1
   // )
-  // await lib.directlyCallAI('xiaoi', {
+  // await lib.directlyCallAI('xiaoi','0x77f0c5deccf15868da16d89dc67c4ab0c6aa0d19', {
   //   question: "你好!"
   // })
-  // await lib.directlyCallAI('baiduOcr', {
+  // await lib.directlyCallAI('baiduOcr', '0xa99782d73844729822603bf1a7574389dd1427fc', {
   //   method: 'idcard',
   //   url: 'http://imgsrc.baidu.com/imgad/pic/item/bd3eb13533fa828bbd0022d9f61f4134970a5aec.jpg'
   // })
